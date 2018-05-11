46e357dda4106fd902c68a960dd492302366e670
caizhengxin <caizhengxin16@163.com>
2018-05-11 16:44:13 +0800

    jankincai

---
+++ README.md
@@ -0,0 +1,19 @@
+## 自启复制u盘数据
+
+## 安装
+
+	pip install -r requirements.txt
+
+## 运行
+
+	python auto_copy.py
+
+## 配置
+
+- DRIVE_LIST：检测盘符列表
+
+- DST_PATH: 复制路径
+
+- EXTENSION：复制什么后缀文件
+
+- FILE_SIZE: 复制小于FILE_SIZE文件
--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # @Author: JanKin Cai
 # @Date:   2018-05-07 16:44:14
 # @Last Modified by:   caizhengxin16@163.com
-# @Last Modified time: 2018-05-11 10:44:25
+# @Last Modified time: 2018-05-11 16:43:45
 
 DEBUG = True
 
@@ -17,9 +17,3 @@ DST_PATH = 'C:\\autocopy'
 EXTENSION = ['.img', '.jpg', '.png', 'jpeg', '.txt']
 
 FILE_SIZE = "10 MB"
-
-UNIT = {
-    "KB": 1024,
-    "MB": 1024 * 1024,
-    "GB": 1024 * 1024 * 1024
-}
